c2727b4412fcb7ab7e687e9e95b6bd4f1e334957
newmohib <newmohib@gmail.com>
2020-03-31 03:07:43 +0600

    pagination finally work complete

--- src/Components/Contents/Pagination/index.js
+++ src/Components/Contents/Pagination/index.js
@@ -3,34 +3,28 @@ import _ from 'lodash';
 
 function Pagination(props) {
 
-    let [pageInfo, setPageInfo] = useState({ pageSize: 2, totalCount: 5, currentPage: 1, pagesCount: 0, pages: [], paginationList: [], isPrevious: "", isNext: "" });
+    let [pageInfo, setPageInfo] = useState({ pageSize: 3, totalCount: 10, currentPage: 1, pagesCount: 0, pages: [], paginationList: [], isPrevious: "", isNext: "" });
 
     useEffect(
         () => {
             const pagesCount = Math.ceil(pageInfo.totalCount / pageInfo.pageSize);
             const pages = _.range(1, pagesCount + 1);
+
             let paginationList = [...pageInfo.paginationList];
-            if (pagesCount > 2 && pagesCount >= pageInfo.currentPage + 4) {
+            if (pagesCount > 2 && pagesCount > pageInfo.currentPage + 2) {
                 paginationList = [pageInfo.currentPage + 1, pageInfo.currentPage + 2, pageInfo.currentPage + 3];
-            }
-            else if (pagesCount > 2 && pagesCount >= pageInfo.currentPage + 3) {
-                paginationList = [pageInfo.currentPage + 1, pageInfo.currentPage + 2];
-            }
-            else if (pagesCount > 2 && pagesCount >= pageInfo.currentPage + 2) {
-                paginationList = [pageInfo.currentPage + 1];
+            } else if (pagesCount <= 2 || pageInfo.currentPage < 2) {
+                paginationList = pagesCount <= 1 ? [1] : [2];
             }
 
-            let isPrevious = pageInfo.currentPage === pagesCount - 1 ? "" : "disabled";
-            let isNext = pages.length === pagesCount ? "" : "disabled";
-
-            //console.log("isPrevious",isPrevious,"isNext",isNext,pageInfo.currentPage ,pagesCount);
-
+            let isPrevious = pageInfo.currentPage === 1 ? "disabled" : "";
+            let isNext = pageInfo.currentPage === pagesCount ? "disabled" : "";
             setPageInfo({ ...pageInfo, pagesCount, pages, paginationList, isPrevious, isNext });
-        }, [pageInfo.currentPage]);
+        },[pageInfo.currentPage]
+    );
 
     const pageChange = (pageNumber => {
         setPageInfo({ ...pageInfo, currentPage: pageNumber });
-        console.log(pageNumber);
     })
     return (
         <div>
